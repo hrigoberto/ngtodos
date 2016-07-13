ca7ca626cae0b521e5eab1364f0c9b0e45506425
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-12 20:04:57 -0400

    finished all features of TodoService

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,18 +1,23 @@
 (function(){
 // is the getter
-  angular.module('ngtodos')
-         .controller('MainController', MainController);
+  angular.module('ngtodos')//this calls up the module which we created called ngtodos
+         .controller('MainController', MainController); //this tells angular to inject the controller known as 'MainController' that is the function MainController, into the app ngtodos
 
   MainController.$inject = ['$scope', 'TodoService'];
+//Tells angular to inject the scope, and TodoService modules into the MainController
+  function MainController($scope, TodoService){ //the function MainController is using both the scope and TodoService modules
+    $scope.message = 'Hey now, what is that sound?' //tells the maincontroller what to replace message with anywhere it is between {{}} within the scope
 
-  function MainController($scope, TodoService){
-    $scope.message = 'Hey now, what is that sound?'
-
-    var todos = TodoService.todos;
-    TodoService.readAll()
-               .then(function(){
+    var todos = TodoService.todos; //declares a variable that is the todos within the TodoService
+    TodoService.readAll() //this tells MainController function to 'read' aka get all of TodoService
+               .then(function(){ // then it'll run a function that logs the todos in the console.
                 todos = TodoService.todos
                 console.log(todos);
               });
+
+  TodoService.create();
+  TodoService.delete();
+  TodoService.update();
+  
   }
 })();
--- public/js/services/TodoService.js
+++ public/js/services/TodoService.js
@@ -1,13 +1,14 @@
 // anonymous self running function (function(){})()
 (function(){
-  angular.module('ngtodos') //calls the angular model ngtodos into this service
-         .factory('TodoService', TodoService); //.factory is one of four types of services. This will be the one we're using for now.
+  angular.module('ngtodos') //calls the angular module ngtodos into this service (known as the getter)
+         .factory('TodoService', TodoService); //.factory is one of four types of services. This will be the one we're using for now. Injects the factory known as TodoService into the ngtodos module;
 
   TodoService.$inject = ['$http']; //injects the module http into TodoService
 
   function TodoService($http){
-    // var o is the object, it has all the functions to call all of the routes
-    var o = {
+
+    var baseUrl = 'https://quiet-refuge-27140.herokuapp.com/' //acts as the baseUrl for functions. Do not forget the '/' at the end.
+    var o = { // var o is the object, it houses variables with all the functions to call all of the routes
       create: createTodo,
       readAll: getAll,
       update: updateTodo,
@@ -16,14 +17,35 @@
     };
     return o;
 
-    function createTodo(){};
-    function getAll(){
-      return $http.get('https://quiet-refuge-27140.herokuapp.com/todos/')
+    function createTodo(desc){
+      var info = {
+        description: desc
+      };
+      return $http.post(baseUrl + 'todos', info)
                   .then(function(response){
-                    o.todos = response.data;
+                    console.log('create', response);
+                    getAll();
                   });
     };
-    function updateTodo(){};
-    function deleteTodo(){};
+    function getAll(){
+      return $http.get(baseUrl + 'todos') // this is telling the http module to go get this route (URL)
+                  .then(function(response){ //then acts as a prompt to start a function once a promise has been fulfilled (in this case the response from the URL is the promise)
+                    o.todos = response.data; //this says that the todos array in object will be made equal to the data in the response from the get
+                  });
+    };
+    function updateTodo(id, newTodo){
+      return $http.put(baseUrl + 'todos' + id, newTodo)
+                  .then(function(response){
+                    console.log('update', response);
+                    getAll();
+                  });
+    };
+    function deleteTodo(id){
+      return $http.delete(baseUrl + 'todos/' + id)
+                  .then(function(response){
+                    console.log('delete', response);
+                    getAll(); //acts as a synchronizer
+                  })
+    };
   }
 })()
